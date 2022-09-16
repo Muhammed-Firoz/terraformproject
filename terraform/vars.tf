ec6d989d5088f0b94a13d85fe3dde60f332c0065
MUHAMMED FIROZ A A <108992155+Muhammed-Firoz@users.noreply.github.com>
2022-09-17 02:25:16 +0530

    Create vars.tf
    variables for main.tf, no default variable specified for VM count

---
+++ terraform/vars.tf
@@ -0,0 +1,32 @@
+variable "packer_resource_group" {
+  description = "Name of the resource group where the packer image is"
+  default     =  "Azuredevops"
+}
+
+variable "prefix" {
+  description = "The prefix which should be used for all resources"
+  default     = "dend-project3"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources should be created."
+  default     = "East US"
+}
+
+variable "username" {
+  description = "The login name of the virtual machines."
+  default     = "firozdendshell"
+  type        = string
+}
+
+variable "password" {
+  description = "The password of the virtual machines."
+  default     = "Ecproject#3"
+  type        = string
+}
+
+variable "num_vms" {
+  description = "The number of VMs to create."
+  type        = number
+}
